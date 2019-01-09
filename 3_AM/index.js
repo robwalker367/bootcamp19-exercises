21c60edb99ac2b1522f5c1d38bbfa6e9fe6e8f12
Rob Walker <robertsrwalker@gmail.com>
2019-01-09 12:56:04 -0500

    3_AM exercise

--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-
-app.use(express.static(__dirname + "/View"));
-app.use(express.static(__dirname + "/Script"));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.get("/grains_used", (req, res) => {
+  res.sendFile(path.join(__dirname + '/grains_used.html'));
+})
+
+app.get("/bread_types", (req, res) => {
+  res.sendFile(path.join(__dirname + '/bread_types.html'));
+})
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname + '/contact.html'));
+})
 
 app.listen(3000);
 console.log("Running at port 3000");
